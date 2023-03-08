58477750ba3b065779fa03ab0245ae31280e5ecc
JonArryn <dumpster@lordofthevale.com>
2023-03-08 09:41:00 -0500

    project setup WIP

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({ greeting: 'Hello world!' });
+});
+
+app.listen(port, () => {
+  console.log(`🚀 server started at http://localhost:${port}`);
+});
