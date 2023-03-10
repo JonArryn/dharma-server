3b3ecb4a5c9380371a8cb2dc9c4b59943bdb9d47
JonArryn <dumpster@lordofthevale.com>
2023-03-10 13:53:36 -0500

    server setup WIP

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ greeting: 'Hello world!' });
